728dc3655ac1bc716947ac797e9e1174156b42ae
Devon Call <callbrentdevon@gmail.com>
2024-07-17 11:52:53 -0700

    WIP

--- gonet.go
+++ gonet.go
@@ -3,7 +3,7 @@ package gonet
 import (
 	"log"
 	"math"
-	"math/rand"
+	"runtime"
 	"sync"
 )
 
@@ -17,8 +17,6 @@ type NN struct {
 	Activations [][]float64
 	// Weights
 	Weights [][][]float64
-	// Last change in weights for momentum
-	Changes [][][]float64
 }
 
 // New creates a new neural network
@@ -69,14 +67,12 @@ func (nn *NN) Config(nInputs int, nHiddens []int, nOutputs int, isRegression boo
 		nn.Activations = append(nn.Activations, vector(nn.NNodes[i], 1.0))
 	}
 	nn.Weights = make([][][]float64, NLayers-1)
-	nn.Changes = make([][][]float64, NLayers-1)
 
 	for i := 0; i < len(nn.Weights); i++ {
 		nn.Weights[i] = matrix(nn.NNodes[i], nn.NNodes[i+1])
-		nn.Changes[i] = matrix(nn.NNodes[i], nn.NNodes[i+1])
 	}
 
-	rand.Seed(0)
+	// rand.Seed(0)
 	for i := 0; i < len(nn.Weights); i++ {
 		for j := 0; j < len(nn.Weights[i]); j++ {
 			for k := 0; k < len(nn.Weights[i][j]); k++ {
@@ -101,45 +97,76 @@ func (nn *NN) feedForward(inputs []float64) []float64 {
 	}
 
 	wg := sync.WaitGroup{}
+	cpuCount := runtime.NumCPU()
+	if cpuCount == 0 {
+		cpuCount = 1
+	}
 	for k := 1; k < NLayers-1; k++ {
-		wg.Add(nn.NNodes[k]-1)
-		for i := 0; i < nn.NNodes[k]-1; i++ {
-			go func(k, i int) {
+		istep := (nn.NNodes[k] - 1) / cpuCount
+		if istep == 0 {
+			istep = (nn.NNodes[k] - 1)
+		} else {
+			wg.Add(cpuCount)
+		}
+		if ((nn.NNodes[k] - 1) % cpuCount) != 0 {
+			wg.Add(1)
+		}
+		for start := 0; start < nn.NNodes[k]-1; start += istep {
+			end := start + istep
+			if end > nn.NNodes[k]-1 {
+				end = nn.NNodes[k] - 1
+			}
+			go func(k, istart, iend int) {
 				defer wg.Done()
-				var sum float64
+				for i := istart; i < iend; i++ {
+					var sum float64
 
-				for j := 0; j < nn.NNodes[k-1]; j++ {
-					sum += nn.Activations[k-1][j] * nn.Weights[k-1][j][i]
-				}
+					for j := 0; j < nn.NNodes[k-1]; j++ {
+						sum += nn.Activations[k-1][j] * nn.Weights[k-1][j][i]
+					}
 
-				if nn.Regression {
-					// Use sigmoid to avoid explosion
-					nn.Activations[k][i] = sigmoid(sum)
-				} else {
-					nn.Activations[k][i] = relu(sum)
+					if nn.Regression {
+						// Use sigmoid to avoid explosion
+						nn.Activations[k][i] = sigmoid(sum)
+					} else {
+						nn.Activations[k][i] = relu(sum)
+					}
 				}
-			}(k, i)
+			}(k, start, end)
 		}
 		wg.Wait()
 	}
 
-
-	wg.Add(nn.NNodes[NLayers-1])
-	for i := 0; i < nn.NNodes[NLayers-1]; i++ {
-		go func(i int) {
+	istep := nn.NNodes[NLayers-1] / cpuCount
+	if istep == 0 {
+		istep = nn.NNodes[NLayers-1]
+	} else {
+		wg.Add(cpuCount)
+	}
+	if nn.NNodes[NLayers-1]%cpuCount != 0 {
+		wg.Add(1)
+	}
+	for start := 0; start < nn.NNodes[NLayers-1]; start += istep {
+		end := start + istep
+		if end > nn.NNodes[NLayers-1] {
+			end = nn.NNodes[NLayers-1]
+		}
+		go func(istart, iend int) {
 			defer wg.Done()
-			var sum float64
+			for i := istart; i < iend; i++ {
+				var sum float64
 
-			for j := 0; j < nn.NNodes[NLayers-2]; j++ {
-				sum += nn.Activations[NLayers-2][j] * nn.Weights[NLayers-2][j][i]
-			}
+				for j := 0; j < nn.NNodes[NLayers-2]; j++ {
+					sum += nn.Activations[NLayers-2][j] * nn.Weights[NLayers-2][j][i]
+				}
 
-			if nn.Regression {
-				nn.Activations[NLayers-1][i] = linear(sum)
-			} else {
-				nn.Activations[NLayers-1][i] = sigmoid(sum)
+				if nn.Regression {
+					nn.Activations[NLayers-1][i] = linear(sum)
+				} else {
+					nn.Activations[NLayers-1][i] = sigmoid(sum)
+				}
 			}
-		}(i)
+		}(start, end)
 	}
 	wg.Wait()
 
@@ -151,7 +178,7 @@ func (nn *NN) feedForward(inputs []float64) []float64 {
 // 'lRate' is learning rate
 // 'mFactor' is used by momentum gradient discent
 // return the prediction error
-func (nn *NN) backPropagate(targets []float64, lRate, mFactor float64) float64 {
+func (nn *NN) backPropagate(targets []float64, lRate float64) float64 {
 	NLayers := len(nn.NNodes)
 	if NLayers < 3 {
 		log.Fatal("Should have at least 1 hidden layer")
@@ -163,48 +190,47 @@ func (nn *NN) backPropagate(targets []float64, lRate, mFactor float64) float64 {
 
 	deltas := make([][]float64, NLayers-1)
 	deltas[NLayers-2] = vector(nn.NNodes[NLayers-1], 0.0)
-	for i := 0; i < nn.NNodes[NLayers-1]; i++ {
+	for node := 0; node < nn.NNodes[NLayers-1]; node++ {
 		if nn.Regression {
-			deltas[NLayers-2][i] = dlinear(nn.Activations[NLayers-1][i]) * (nn.Activations[NLayers-1][i] - targets[i])
+			deltas[NLayers-2][node] = dlinear(nn.Activations[NLayers-1][node]) * (nn.Activations[NLayers-1][node] - targets[node])
 		} else {
-			deltas[NLayers-2][i] = dsigmoid(nn.Activations[NLayers-1][i]) * (nn.Activations[NLayers-1][i] - targets[i])
+			deltas[NLayers-2][node] = dsigmoid(nn.Activations[NLayers-1][node]) * (nn.Activations[NLayers-1][node] - targets[node])
 		}
 	}
 
 	wg := sync.WaitGroup{}
-	for k := len(deltas) - 2; k >= 0; k-- {
-		deltas[k] = vector(nn.NNodes[k+1], 0.0)
-		wg.Add(nn.NNodes[k+1])
-		for i := 0; i < nn.NNodes[k+1]; i++ {
-			go func(k, i int) {
+	for layer := len(deltas) - 2; layer >= 0; layer-- {
+		deltas[layer] = vector(nn.NNodes[layer+1], 0.0)
+		wg.Add(nn.NNodes[layer+1])
+		for node := 0; node < nn.NNodes[layer+1]; node++ {
+			go func(layer, node int) {
 				defer wg.Done()
-				var e float64
+				var expect float64
 
-				for j := 0; j < nn.NNodes[k+2]-1; j++ {
-					e += deltas[k+1][j] * nn.Weights[k+1][i][j]
+				for nextNode := 0; nextNode < nn.NNodes[layer+2]-1; nextNode++ {
+					expect += deltas[layer+1][nextNode] * nn.Weights[layer+1][node][nextNode] //
 				}
 
 				if nn.Regression {
-					deltas[k][i] = dsigmoid(nn.Activations[k+1][i]) * e
+					deltas[layer][node] = dsigmoid(nn.Activations[layer+1][node]) * expect
 				} else {
-					deltas[k][i] = drelu(nn.Activations[k+1][i]) * e
+					deltas[layer][node] = drelu(nn.Activations[layer+1][node]) * expect
 				}
-			}(k, i)
+			}(layer, node)
 		}
 		wg.Wait()
 	}
 
-	for k := NLayers - 2; k >= 0; k-- {
-		wg.Add(nn.NNodes[k])
-		for i := 0; i < nn.NNodes[k]; i++ {
-			go func(k, i int) {
+	for layer := NLayers - 2; layer >= 0; layer-- {
+		wg.Add(nn.NNodes[layer])
+		for node := 0; node < nn.NNodes[layer]; node++ {
+			go func(layer, node int) {
 				defer wg.Done()
-				for j := 0; j < nn.NNodes[k+1]; j++ {
-					change := deltas[k][j] * nn.Activations[k][i]
-					nn.Weights[k][i][j] = nn.Weights[k][i][j] - lRate*(change+mFactor*nn.Changes[k][i][j])
-					nn.Changes[k][i][j] = change
+				for nextLayerNode := 0; nextLayerNode < nn.NNodes[layer+1]; nextLayerNode++ {
+					change := deltas[layer][nextLayerNode] * nn.Activations[layer][node]
+					nn.Weights[layer][node][nextLayerNode] = nn.Weights[layer][node][nextLayerNode] - lRate*change
 				}
-			}(k, i)
+			}(layer, node)
 		}
 		wg.Wait()
 	}
@@ -226,13 +252,13 @@ func (nn *NN) backPropagate(targets []float64, lRate, mFactor float64) float64 {
 // 'mFactor' is used by momentum gradient discent
 //
 // 'debug' is whether or not to log learning error every 1000 iterations
-func (nn *NN) Train(inputs [][][]float64, iterations int, lRate, mFactor float64, debug bool) {
+func (nn *NN) Train(inputs [][][]float64, iterations int, lRate float64, debug bool) {
 	for i := 1; i <= iterations; i++ {
 		var e float64
 		for _, p := range inputs {
 			nn.feedForward(p[0])
 
-			tmp := nn.backPropagate(p[1], lRate, mFactor)
+			tmp := nn.backPropagate(p[1], lRate)
 			e += tmp
 		}
 		if debug && i%1000 == 0 {
--- gonet_test.go
+++ gonet_test.go
@@ -2,10 +2,16 @@ package gonet
 
 import (
 	"math"
+	"math/rand"
+	"runtime"
 	"testing"
 )
 
+const testIterations = 5000
+
 func TestPredict(t *testing.T) {
+	cpus := runtime.NumCPU()
+	t.Logf("Number of CPUs: %d", cpus)
 	// AND test
 	andTest := [][][]float64{
 		{{0, 0}, {0}},
@@ -13,8 +19,9 @@ func TestPredict(t *testing.T) {
 		{{1, 0}, {0}},
 		{{1, 1}, {1}},
 	}
-	nn := New(2, []int{4}, 1, false)
-	nn.Train(andTest, 3000, 0.4, 0.2, false)
+	rand.Seed(0)
+	nn := New(2, []int{cpus}, 1, false)
+	nn.Train(andTest, testIterations, 0.4, false)
 	for i := 0; i < len(andTest); i++ {
 		input := andTest[i][0]
 		output := andTest[i][1]
@@ -31,8 +38,9 @@ func TestPredict(t *testing.T) {
 		{{1, 0}, {1}},
 		{{1, 1}, {1}},
 	}
-	nn.Config(2, []int{4}, 1, false)
-	nn.Train(orTest, 3000, 0.4, 0.2, false)
+	rand.Seed(0)
+	nn.Config(2, []int{cpus}, 1, false)
+	nn.Train(orTest, testIterations, 0.4, false)
 	for i := 0; i < len(orTest); i++ {
 		input := orTest[i][0]
 		output := orTest[i][1]
@@ -49,8 +57,9 @@ func TestPredict(t *testing.T) {
 		{{1, 0}, {1}},
 		{{1, 1}, {0}},
 	}
-	nn.Config(2, []int{4}, 1, false)
-	nn.Train(xorTest, 3000, 0.4, 0.2, false)
+	rand.Seed(0)
+	nn.Config(2, []int{cpus}, 1, false)
+	nn.Train(xorTest, testIterations, 0.4, false)
 	for i := 0; i < len(xorTest); i++ {
 		input := xorTest[i][0]
 		output := xorTest[i][1]
@@ -67,14 +76,16 @@ func TestPredict(t *testing.T) {
 		{{5}, {10}},
 		{{6}, {12}},
 	}
-	nn.Config(1, []int{3, 3}, 1, true)
-	nn.Train(regTest, 3000, 0.6, 0.4, false)
+	rand.Seed(0)
+	nn.Config(1, []int{cpus, cpus}, 1, true)
+	nn.Train(regTest, testIterations, 0.6, false)
+	t.Logf("model: %+v", nn)
 	for i := 0; i < len(regTest); i++ {
 		input := regTest[i][0]
 		output := regTest[i][1]
 		predict := nn.Predict(input)
 		if math.Round(predict[0]) != output[0] {
-			t.Errorf("Regression test failed. Got %f with input %f, want %f.", math.Round(predict[0]), input[0], output[0])
+			t.Errorf("Regression test failed. Got %f (rounded from %f) with input %f, want %f.", math.Round(predict[0]), predict[0], input[0], output[0])
 		}
 	}
 }
